e1e9de28ca2058fb5dd8b5df596bd29df8ef5bc2
CasterV <792684107@qq.com>
2016-11-29 21:37:33 +0800

    Fix the bug that simulation context is not released during closing.

--- CasterUnitCore/SimulationContext/CapeMaterialTemplateSystem.cs
+++ CasterUnitCore/SimulationContext/CapeMaterialTemplateSystem.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -19,11 +20,24 @@ namespace CasterUnitCore
 
         public static string[] MaterialTemplates
         {
-            get { return (_materialTemplateSystem.MaterialTemplates as IEnumerable<string>).ToArray(); }
+            get
+            {
+                if (_materialTemplateSystem == null)
+                {
+                    Debug.WriteLine("Get MaterialTemplates Failed.");
+                    return new string[0];
+                }
+                return (_materialTemplateSystem.MaterialTemplates as IEnumerable<string>).ToArray();
+            }
         }
 
         public static ICapeThermoMaterialTemplate CreateMaterialTemplate(string materialTemplateName)
         {
+            if (_materialTemplateSystem == null)
+            {
+                Debug.WriteLine("Create MaterialTemplate Failed.");
+                return null;
+            }
             return _materialTemplateSystem.CreateMaterialTemplate(materialTemplateName);
         }
     }
